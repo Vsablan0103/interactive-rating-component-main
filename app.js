7eefd55a37e2516bbf8f1d8e5d37a630d699c879
Vsablan0103 <sablan.vincent13@gmail.com>
2022-05-27 18:10:57 +0800

    finished project

---
+++ app.js
@@ -0,0 +1,25 @@
+const numbers = document.querySelectorAll('.number');
+const submitButton = document.querySelector('button[type="submit"]');
+const thankyouCard = document.querySelector('.hide');
+const ratingSpan = document.querySelector('#rating-value');
+
+numbers.forEach(num => {
+    num.addEventListener('click', function () {
+        if (![...numbers].some(num => num.classList.contains('selected'))) {
+            this.classList.add('selected');
+        } else {
+            numbers.forEach(num => num.classList.remove('selected'));
+            this.classList.add('selected');
+        }
+    })
+})
+
+submitButton.addEventListener('click', () => {
+    const selectedNumber = [...numbers].filter(num => num.classList.contains('selected'));
+    let valueOfSelected = selectedNumber[0].dataset.value;
+
+    thankyouCard.classList.add('thankyou-card');
+
+    ratingSpan.innerText = valueOfSelected;
+
+})
